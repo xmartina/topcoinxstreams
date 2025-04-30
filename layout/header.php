e64d13cb4060e86d283249c6a3f7bab4228d6754
xmartina <martinsprince126@gmail.com>
2025-04-30 11:08:02 +0100

    d

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>itpro - Finance Consulting HTML5 Template</title>
+    <title><?=$pageName?> | <?=$siteName?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
