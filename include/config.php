757d929903e9a45e78bef8189901c36148159b17
xmartina <martinsprince126@gmail.com>
2025-04-30 14:14:23 +0100

    d

--- include/config.php
+++ include/config.php
@@ -5,4 +5,5 @@ $support_plugin = getenv('SUPPORT_PLUGIN');
 $siteLink = 'https://topcoinxstreams.com';
 $accountLink = 'https://account.topcoinxstreams.com';
 $siteName  = 'Topcoin Xstreams';
-$siteEmail  = 'contact@topcoinxstreams.com';
+$siteEmails = array('contact@topcoinxstreams.com', 'support@topcoinxstreams.com');
+
---
+++ pages/contactpage.php
@@ -0,0 +1,122 @@
+<?php
+$pageName = 'Terms';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/config.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/header.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/breadcrumb.php';
+?>
+
+
+<!--==================================================-->
+<!-- Start Appoinment Section -->
+<!--===================================================-->
+<div class="contact-us pt-90 pb-90">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-6 pl-0 pr-0">
+                <div class="contact_from_box">
+                    <div class="contact_title pb-4">
+                        <h3>Get In Touch</h3>
+                    </div>
+                    <form action="#" method="POST" id="dreamit-form">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="form_box mb-30">
+                                    <input type="text" name="name" placeholder="Name">
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form_box mb-30">
+                                    <input type="email" name="email" placeholder="Email Address">
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form_box mb-30">
+                                    <input type="text" name="phone" placeholder="Phone Number">
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form_box mb-30">
+                                    <input type="text" name="web" placeholder="Website">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12">
+                                <div class="form_box mb-30">
+                                    <textarea name="message" id="message" cols="30" rows="10"
+                                              placeholder="Your Message"></textarea>
+                                </div>
+                                <div class="quote_button">
+                                    <button class="btn" type="submit"> <i class="bi bi-gear"></i> Free Consultation
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <div id="status"></div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-6 pl-0 pr-0">
+                <div class="cda-content-area">
+                    <div class="cda-single-content d-flex">
+                        <div class="cda-icon">
+                            <i class="fas fa-map-marker-alt"></i>
+                        </div>
+                        <div class="cda-content-inner">
+                            <h4>Company Location</h4>
+                            <p>Durham Street Hialeah, <br>FL 33010, USA</p>
+                        </div>
+                    </div>
+                    <div class="cda-single-content hr d-flex">
+                        <div class="cda-icon">
+                            <i class="fas fa-phone-alt"></i>
+                        </div>
+                        <div class="cda-content-inner">
+                            <h4>Telephone Number</h4>
+                            <p>+880 636 524 265, <br>+880 636 524 265, </p>
+                        </div>
+                    </div>
+                    <div class="cda-single-content hr d-flex">
+                        <div class="cda-icon">
+                            <i class="fas fa-envelope"></i>
+                        </div>
+                        <div class="cda-content-inner">
+                            <h4>Our Email Address</h4>
+                            <?php
+                            foreach ($siteEmail as $email){
+                            ?>
+                            <p><?=$email?><br></p>
+                            <?php } ?>
+                        </div>
+                    </div>
+                    <div class="cda-single-content hr d-flex">
+                        <div class="cda-icon">
+                            <i class="fas fa-clock"></i>
+                        </div>
+                        <div class="cda-content-inner">
+                            <h4>Business Hours</h4>
+                            <p>Monday - Friday: 9 AM - 6 PM</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--==================================================-->
+<!-- Start Contact Location Section -->
+<!--===================================================-->
+<div class="map-section">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 p-0">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48384.367867189205!2d-74.01058227968896!3d40.71751035716885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1609671967457!5m2!1sen!2sbd"
+                    width="1920" height="350" style="border:0;" allowfullscreen="" aria-hidden="false"
+                    tabindex="0"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/footer.php'; ?>
