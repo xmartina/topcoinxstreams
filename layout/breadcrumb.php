e7f386d31ee4a52e5c979cedc37ba1151f1829f6
xmartina <martinsprince126@gmail.com>
2025-04-30 12:38:16 +0100

    d

--- layout/breadcrumb.php
+++ layout/breadcrumb.php
@@ -1,1 +1,26 @@
-<?php
+<!--==================================================-->
+<!--  Start Header Slider Section -->
+<!--===================================================-->
+<div class="breadcumb-area d-flex align-items-center">
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-lg-12">
+                <div class="breadcumb-content">
+                    <h1>About Us</h1>
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li>About Us </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="britcam-shape">
+                <div class="breadcumb-content upp">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li>About Us </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ pages/aboutpage.php
@@ -0,0 +1,11 @@
+<?php
+$pageName = 'Home';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/config.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/header.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/breadcrumb.php';
+
+?>
+
+
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/footer.php'; ?>
