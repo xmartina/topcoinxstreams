8d57cf2a92b00d643aae2c02c5632c8a061096d7
xmartina <martinsprince126@gmail.com>
2025-04-30 10:47:37 +0100

    d

---
+++ include/config.php
@@ -0,0 +1,6 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
+$support_plugin = getenv('SUPPORT_PLUGIN');
+
+$siteLink = 'https://topcoinxstreams.com';
+$siteName  = 'Topcoin Xstreams';
