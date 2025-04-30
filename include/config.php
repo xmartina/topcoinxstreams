17c8d8147bfa2854ba570778219852c2ac19227b
xmartina <martinsprince126@gmail.com>
2025-04-30 20:13:52 +0100

    d

--- include/config.php
+++ include/config.php
@@ -1,6 +1,21 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
-$support_plugin = getenv('SUPPORT_PLUGIN');
+$smartsuppKey = getenv('SMARTSUPP_KEY');
+$support_plugin = <<<HTML
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '{$smartsuppKey}';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+HTML;
+
 
 $siteLink = 'https://topcoinxstreams.com';
 $accountLink = 'https://account.topcoinxstreams.com';
